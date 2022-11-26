616f3321826f16351139139fbd38762b46e2f1af
riki-oyashima <a.m.4jgfdalkgphe@gmail.com>
2022-11-26 22:49:04 +0900

    [modify]サーバをDjangoからFlaskに変更。

--- ev_sys_pwa/build.sh
+++ ev_sys_pwa/build.sh
@@ -36,8 +36,8 @@ proj=`cat ../project.txt`
 gsutil cp -rp gs://${proj}-sa-master/service.json .
 
 # DebugモードをFalseに設定
-debug_before=`grep -e "^DEBUG\s*=" api/settings.py`
-sed -i -e "s/^DEBUG\s*=.*/DEBUG = False/g" api/settings.py
+#debug_before=`grep -e "^DEBUG\s*=" api/settings.py`
+#sed -i -e "s/^DEBUG\s*=.*/DEBUG = False/g" api/settings.py
 
 # build実行
 image_name="asia.gcr.io/${proj}/evaluation-system-server"
@@ -54,5 +54,5 @@ fi
 rm -rf service.json
 
 # Debugモードを変更前の値に巻き戻し
-sed -i -e "s/^DEBUG\s*=.*/${debug_before}/g" api/settings.py
+#sed -i -e "s/^DEBUG\s*=.*/${debug_before}/g" api/settings.py
 
